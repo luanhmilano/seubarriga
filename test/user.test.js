b7da2cf0b464dfccab1cbf55eeb88f5fba134a23
Luan Henrique <luanhsantos14@gmail.com>
2024-04-26 11:51:48 -0300

    devo inserir usuário com sucesso

--- test/user.test.js
+++ test/user.test.js
@@ -8,3 +8,9 @@ test('Deve listar todos os usuários', async () => {
   expect(res.body).toHaveLength(1) 
   expect(res.body[0]).toHaveProperty('name', 'John Doe') 
 })
+
+test('Deve inserir usuário com sucesso', async () => {
+  const res = await request(app).post('/users').send({ name: 'Walter White', mail: 'walterw@gmail.com' })
+  expect(res.status).toBe(201)
+  expect(res.body.name).toBe('Walter White')
+})
