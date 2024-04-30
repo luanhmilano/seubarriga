5a0e2d4c27b0a5f4cba05907eead8eb22370d20d
Luan Henrique <luanhsantos14@gmail.com>
2024-04-30 12:06:59 -0300

    fix: fix database connection

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+const { host, password, database, port } = require("pg/lib/defaults");
+
+module.exports = {
+    test: {
+        client: 'pg',
+        version: '9.6',
+        connection: {
+            host: 'localhost',
+            port: '5433',
+            user: 'postgres',
+            password: 'postgres',
+            database: 'barriga'
+        },
+        migrations: {
+            directory: 'src/migrations'
+        }
+    }
+}
