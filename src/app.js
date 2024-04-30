c017460076fbc99f3551d9a10106fc53ccae32d5
Luan Henrique <luanhsantos14@gmail.com>
2024-04-30 14:38:59 -0300

    devo refatorar para usar banco de dados

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const knex = require('knex')
+const knexfile = require('../knexfile')
+
+// TODO criar chaveamento dinâmico
+app.db = knex(knexfile.test)
 
 const consign = require('consign')
 
--- test/routes/user.test.js
+++ test/routes/user.test.js
@@ -1,16 +1,17 @@
 const request = require('supertest')
 
-const app = require('../src/app')
+const app = require('../../src/app')
+const { password } = require('pg/lib/defaults')
 
 test('Deve listar todos os usuários', async () => {
   const res = await request(app).get('/users')
   expect(res.status).toBe(200)
-  expect(res.body).toHaveLength(1) 
-  expect(res.body[0]).toHaveProperty('name', 'John Doe') 
+  expect(res.body.length).toBeGreaterThan(0)
 })
 
 test('Deve inserir usuário com sucesso', async () => {
-  const res = await request(app).post('/users').send({ name: 'Walter White', mail: 'walterw@gmail.com' })
+  const email = `${Date.now()}@gmail.com`
+  const res = await request(app).post('/users').send({ name: 'Walter White', email, passwd: '123456' })
   expect(res.status).toBe(201)
   expect(res.body.name).toBe('Walter White')
 })
