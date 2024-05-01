9689df07d715fd8494b5a3ecb5a73997d495c677
Luan Henrique <luanhsantos14@gmail.com>
2024-05-01 10:16:40 -0300

    não deve inserir uma conta sem nome

--- test/routes/account.test.js
+++ test/routes/account.test.js
@@ -16,12 +16,23 @@ test('Deve inserir uma nova conta com sucesso', () => {
     })
 })
 
+test('Não deve inserir uma conta sem nome', () => {
+    return request(app).post(MAIN_ROUTE).send({ user_id: user.id }).then((result) => {
+        expect(result.status).toBe(400)
+        expect(result.body.error).toBe('Nome é um atributo obrigatório')
+    })
+})
+
+test.skip('Não deve inserir uma conta de nome duplicado, para o mesmo usuário', () => {})
+
 test('Deve listar todas as contas', () => {
     return app.db('accounts').insert({ name: 'Acc list', user_id: user.id }).then(() => request(app).get(MAIN_ROUTE)).then((res) => {
         expect(res.status).toBe(200)
         expect(res.body.length).toBeGreaterThan(0)
     })
 })
+
+test.skip('Deve listar apenas as contas do usuário', () => {})
 
 test('Deve retornar uma conta por Id', () => {
     return app.db('accounts').insert({ name: 'Acc By Id', user_id: user.id }, ['id']).then(acc => request(app).get(`${MAIN_ROUTE}/${acc[0].id}`)).then((res) => {
@@ -31,6 +42,8 @@ test('Deve retornar uma conta por Id', () => {
     })
 })
 
+test.skip('Não deve retornar uma conta de outro usuário', () => {})
+
 test('Deve alterar uma conta', () => {
     return app.db('accounts').insert({ name: 'Acc to Update', user_id: user.id }, ['id']).then(acc => request(app).put(`${MAIN_ROUTE}/${acc[0].id}`).send({name: 'Acc Updated'})).then((res) => {
         expect(res.status).toBe(200)
@@ -38,8 +51,12 @@ test('Deve alterar uma conta', () => {
     })
 })
 
+test.skip('Não deve alterar uma conta de outro usuário', () => {})
+
 test('Deve remover uma conta', () => {
     return app.db('accounts').insert({ name: 'Acc to remove', user_id: user.id }, ['id']).then(acc => request(app).delete(`${MAIN_ROUTE}/${acc[0].id}`)).then((res) => {
         expect(res.status).toBe(204)
     })
 })
+
+test.skip('Não deve remover uma conta de outro usuário', () => {})
