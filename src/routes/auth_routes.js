8ac931df137c2e7ec1eb0aa17b3dfff0b4b7b477
Luan Henrique <luanhsantos14@gmail.com>
2024-05-02 16:41:04 -0300

    não deve autenticar usuário com senha errada

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,10 +1,12 @@
 const jwt = require('jwt-simple')
 const bcrypt = require('bcrypt-nodejs')
+const ValidationError = require('../errors/ValidationError')
 const secret = 'Segredo!'
 
 module.exports = (app) => {
     const signin = (req, res, next) => {
         app.services.user.findOne({ email: req.body.email }).then((user) => {
+            if (!user) throw new ValidationError('Usuário ou senha inválido')
             if(bcrypt.compareSync(req.body.passwd, user.passwd)) {
                 const payload = {
                     id: user.id,
@@ -13,7 +15,7 @@ module.exports = (app) => {
                 }
                 const token = jwt.encode(payload, secret)
                 res.status(200).json({ token })
-            }
+            } else throw new ValidationError('Usuário ou senha inválido')
         }).catch(err => next(err))
     }
 
--- test/routes/auth.test.js
+++ test/routes/auth.test.js
@@ -8,3 +8,18 @@ test('Deve receber token ao logar', () => {
         expect(res.body).toHaveProperty('token')
     })
 })
+
+test('Não deve autenticar usuário com senha errada', () => {
+    const email = `${Date.now()}@gmail.com`
+    return app.services.user.save({ name: 'Gus', email, passwd: '123456' }).then(() => request(app).post('/auth/signin').send({ email, passwd: '654321' })).then((res) => {
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe('Usuário ou senha inválido')
+    })
+})
+
+test('Não deve autenticar usuário inexistente', () => {
+    return request(app).post('/auth/signin').send({ email: 'naoExiste@gmail.com', passwd: '654321' }).then((res) => {
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe('Usuário ou senha inválido')
+    })
+})
