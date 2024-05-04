567aa79c6e4dbc97c24380dc556f5e24bf7ef247
Luan Henrique <luanhsantos14@gmail.com>
2024-05-04 18:53:30 -0300

    ao tentar salvar uma transferência inválida

--- src/routes/transfers_routes.js
+++ src/routes/transfers_routes.js
@@ -3,11 +3,17 @@ const express = require('express')
 module.exports = (app) => {
     const router = express.Router()
 
+    const validate = (req, res, next) => {
+        app.services.transfer.validate({ ...req.body, user_id: req.user.id })
+          .then(() => next())
+          .catch(err => next(err));
+    };
+
     router.get('/', (req, res, next) => {
         app.services.transfer.find({user_id: req.user.id}).then(result => res.status(200).json(result)).catch(err => next(err))
     })
 
-    router.post('/', (req, res, next) => {
+    router.post('/', validate, (req, res, next) => {
         const transfer = { ...req.body, user_id: req.user.id };
         app.services.transfer.save(transfer)
           .then(result => res.status(201).json(result[0]))
